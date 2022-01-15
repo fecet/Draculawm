46de70a29a3ff365dbea7c7f934d9cdb76388be3
fecet <xiezej@gmail.com>
2022-01-16 03:44:08 +0800

    change tags

--- config.h
+++ config.h
@@ -51,7 +51,8 @@ static const char *const autostart[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", " ", "", "", "\uf537", "\uf684" };
+/* static const char *tags[] = { "", "", "", "", " ", "", "", "\uf537", "\uf684" }; */
+static const char *tags[] = { "1", "2", "3", "4", "5 ", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
